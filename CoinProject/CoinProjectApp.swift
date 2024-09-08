55971a889365c40e08f4103aa750728cd8410890
JunYeong-J <eeot2827@gmail.com>
2024-09-08 20:30:49 +0900

    Initial Commit

---
+++ CoinProject/CoinProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinProjectApp.swift
+//  CoinProject
+//
+//  Created by 전준영 on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
